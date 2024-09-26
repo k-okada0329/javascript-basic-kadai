607175e8c080fabb02c6b7b88e7bba1e6d06a0a8
k-okada0329 <k_okada0329@yahoo.co.jp>
2024-09-26 19:49:26 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,3 @@
 let today = new Date();
 
-console.log(today.getFullYear() + '年' + today.getMonth() + '月' + today.getDay() + '日');
+console.log(today.getFullYear() + '年' + (today.getMonth()+1) + '月' + today.getDate() + '日');
